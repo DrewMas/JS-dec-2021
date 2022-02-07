9b24b98deafe3f5e655b3bcbbfd6d8ae1ff483d7
DrewMas <andrewmasnyi@gmail.com>
2022-02-07 18:36:53 +0200

    hw3 - done

--- main.js
+++ main.js
@@ -115,3 +115,10 @@
 //        document.write(`<div>The index is - ${i}</div>`)
 //    }
 // }
+// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// for (let i = 0; i < 100; i++) {
+//    if (i%2 === 1){
+//        console.log(i);
+//        document.write(`<div>The index is - ${i}</div>`)
+//    }
+// }
